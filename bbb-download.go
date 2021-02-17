a05af99fa3f6c3875bb817db8b0e6926df80d9bc
Zafer Akçalı <zaferakcali@yahoo.com>
2021-02-17 14:51:49 +0300

    manages svg images, webcams webm/mp4

--- bbb-download.go
+++ bbb-download.go
@@ -14,7 +14,12 @@ import (
 	"strings"
 )
 
+var webcamsFile = "webcams.webm"
+var slidesFile = "slides.mp4"
+var nSlides int =1
+
 func main () {
+
 	fmt.Println ("Bigbluebutton video creator/downloader")
 	fmt.Print ("Enter url of conference/lecture: ")
 	scanner := bufio.NewScanner(os.Stdin)
@@ -25,10 +30,12 @@ func main () {
 
 	result2 := strings.Split(result [0], "/playback/" )
 	baseUrl := result2 [0] + "/presentation/" + presentationId
-	fmt.Println ("baseUrl= ", baseUrl)
+	shapesUrl := baseUrl + "/shapes.svg"
+	webcamsUrl := baseUrl + "/video/"
+	metaUrl := baseUrl + "/metadata.xml"
+//	fmt.Println ("baseUrl= ", baseUrl)
 
 	//read duration of recording and meeting name from meta.xml
-	metaUrl := baseUrl + "/metadata.xml"
     responseMeta, err := http.Get(metaUrl)
     if err != nil {
         log.Fatal(err)
@@ -47,9 +54,7 @@ func main () {
     meetingString := strings.SplitAfter(string(metaBody),"<meetingName>")
     meetingName := strings.Split(meetingString[1],"</meetingName>")
     fmt.Println ("name of the meeting=", meetingName[0])
-		
-	shapesUrl := baseUrl + "/shapes.svg"
-	webcamsUrl := baseUrl + "/video/webcams.webm"
+
 
 	//read content of the shapes.svg file, and assign shapes to it
     responseShapes, err := http.Get(shapesUrl)
@@ -68,81 +73,103 @@ func main () {
 	if _, err := os.Stat(presentationId); os.IsNotExist(err) {
     os.Mkdir(presentationId, 0700) // create temporary dir
 }
+
+// download webcams
+	fmt.Print ("downloading webcams",  "\r")
+	if err := DownloadFile(presentationId+"/"+webcamsFile, webcamsUrl+"/"+webcamsFile); err != nil {
+		panic(err) 	}
+	fmt.Println (webcamsFile, " file is downloaded",  "\r")
+	fi, err := os.Stat(presentationId+"/"+webcamsFile)
+		if err != nil {
+    		panic(err)
+			}
+	fileSize := fi.Size()
+//	fmt.Print ("file size =", fileSize,  "\r")
+	if fileSize<1000 { // returned 404 error text
+// webcams.webm file is so small that real webcams file must be in mp4 format
+		webcamsFile = "webcams.mp4"
+		fmt.Print ("downloading webcams", "\r")
+		if err := DownloadFile(presentationId+"/"+webcamsFile, webcamsUrl+"/"+webcamsFile); err != nil {
+			panic(err) 	}
+		fmt.Println (webcamsFile, "file is downloaded",  "\r")
+	}
+
      // Find and print slide timings, image Urls
 	durations := make(map[int]float64)
 	vidnames := make(map[int]string)
 	imgnames := make(map[int]string)
-	inValue, outValue, truncated:= 0.0, 0.0, 0.0
-	inSrc, outSrc, pngSrc, lastPNG := "0.0", "10.5", "presentation/", "s1.png"  
+	inValue,  outValue, videoLength, truncated:= 0.0, 0.0, 0.0, 0.0
+	inSrc, outSrc, pngSrc := "0.0", "10.5", "presentation/"  
 	i:=1 // number of png pictures for slide
 
 	//parse for in= out= href= from /shapes.svg
 	ins := strings.Split(shapes, "in=\"")
-	outs := strings.Split(shapes, "out=\"")
-	pngs := strings.Split(shapes, "xlink:href=\"")
 
+	fmt.Println ("Downloading Slides")
+// download all off the slides loop
 	for k :=1; k < len (ins); k++ {
 	intext := strings.SplitAfter (ins[k], "\"")
 	realin:=strings.Split(intext[0],"\"")
 	inSrc = (realin[0])
 
-	outtext := strings.SplitAfter (outs[k], "\"")
-	realout:=strings.Split(outtext[0],"\"")
+	outtext := strings.SplitAfter (ins[k], "out=\"")
+	realout:=strings.Split(outtext[1],"\"")
 	outSrc = (realout[0])
 
-	imgtext := strings.SplitAfter (pngs[k], "\"")
-	realpng:=strings.Split(imgtext[0],"\"")
+	imgtext := strings.SplitAfter (ins[k], "xlink:href=\"")
+	realpng:=strings.Split(imgtext[1],"\"")
 	pngSrc = (realpng[0])
-	// if a poll made, then image file is not a PNG
-	isSVG := strings.Contains(pngSrc, ".svg")
-	// use last known PNG
-	if isSVG == false {lastPNG = pngSrc
-		} else {pngSrc = lastPNG }
 
 	inValue, _ = strconv.ParseFloat (inSrc,64)
 	outValue, _ = strconv.ParseFloat (outSrc,64)
-	truncated = (outValue*10-inValue*10)/10
+	truncated = ( outValue*10-inValue*10)/10
 	durations[i] = truncated
 	imgnames [i] = "s" + strconv.Itoa(i)+".png"
 	vidnames[i] = "v" + strconv.Itoa(i) + ".mp4"
 
 	imgUrl := baseUrl + "/" + pngSrc
-//	fmt.Println (inSrc, " ", outSrc, " ", durations[i], " ", pngSrc, isSVG, imgnames [i], " ", vidnames[i])
-
+//	fmt.Println (inSrc, " ", outSrc, " ", durations[i], " ", pngSrc, imgnames [i], " ", vidnames[i])
+	fmt.Print ("Downloading: ", imgnames [i], "\r") // print to same line just like a counter
 		if err := DownloadFile(presentationId+"/"+imgnames[i], imgUrl); err != nil {
 				panic(err)
 			}	
 	i++
 	}
+nSlides=i-1  // if only one slide is converted, than nSlides = 1
+fmt.Println("Number of slides =",nSlides)
+	// end of download all off the slides loop
 		//correct duration of last slide
 		outValue, _ = strconv.ParseFloat(duration[0],64)
-		outValue=outValue/1000
-		outValue=math.Round (outValue*100)/100
-		fmt.Println("ending of presentation=", outValue) 
-		truncated = (outValue*10-inValue*10)/10
+		outValue= outValue/1000
+		videoLength=math.Round ( outValue*100)/100
+		fmt.Println("Length of presentation =",  videoLength) 
+		truncated = ( videoLength*10-inValue*10)/10
 		durations[i-1] = math.Round (truncated*100)/100
-		fmt.Println("Duration of last slide according to meta.xml is: ", durations[i-1]) 
+		fmt.Println("Duration of last slide according to meta.xml =", durations[i-1]) 
 	
 		// create mp4 files from png files
 fmt.Println ("Creating videos from slide pictures, duration is given as seconds")
-	for j:=1; j<i; j++ 	{
+	for j:=1; j<=nSlides; j++ 	{
 	fmt.Print (imgnames[j], " ", vidnames[j], " ", durations [j], "\r") // print to same line just like a counter
-
 	cmd := exec.Command("ffmpeg","-loop", "1", "-r", "5", "-f", "image2", 
 						"-i", presentationId +"/"+imgnames[j],
 						"-c:v", "libx264", "-r", "24", "-t", fmt.Sprint(durations[j]), "-pix_fmt", "yuv420p", 
 						"-vf", "scale=860:644",        // as close as 800x600
 						presentationId+"/"+vidnames[j]  )
 	cmd.Run()
+
 	}
-	
+
+if nSlides == 1 { 
+			slidesFile="v1.mp4" // none of the videos are merged
+} else { // there are more than one video file, so merge them	
 	//create video_list.txt file to cancat with ffmpeg
 	    f, err := os.Create("video_list.txt")
     if err != nil {
         fmt.Println(err)
         return
     }
-	for j:=1; j<i; j++ 	{
+	for j:=1; j<=nSlides; j++ 	{
 	    _, err := f.WriteString("file " + presentationId+"/"+vidnames[j] + "\n")
     if err != nil {
         fmt.Println(err)
@@ -154,33 +181,32 @@ fmt.Println ("Creating videos from slide pictures, duration is given as seconds"
         fmt.Println(err)
         return }
 	//concat slide videos to create one piece of video file: slides.mp4
+
 fmt.Println ("merging slide videos to create: slides.mp4")
 	cmd := exec.Command("ffmpeg","-f", "concat", "-safe", "0", "-i", "video_list.txt", 
-						"-c", "copy", presentationId+"/"+"slides.mp4")
+						"-c", "copy", presentationId+"/"+slidesFile)
 	cmd.Run()
 fmt.Println ("slide videos merged")
-// download webcams.webm
-fmt.Println ("downloading webcams")
-	if err := DownloadFile(presentationId+"/"+"webcams.webm", webcamsUrl); err != nil {
-		panic(err) 	}
-	fmt.Println ("webcams.webm file is downloaded")
-//convert webcams.webm to webcams.mp4
-	fmt.Println ("converting webcams.webm to webcams.mp4")
-	cmd = exec.Command("ffmpeg","-i", presentationId+"/"+"webcams.webm",
+
+} 
+
+//convert webcams video file to 644p webcams644.mp4
+fmt.Println ("converting 480p ",webcamsFile, " to 644p webcams644.mp4")
+	cmd := exec.Command("ffmpeg","-i", presentationId+"/"+webcamsFile,
 						 "-q:a", "0", "-q:v", "0", 
 						 "-vf", "scale=420:-2,pad=height=644", 
-						presentationId+"/"+"webcams.mp4")
+						presentationId+"/"+"webcams644.mp4")
 	cmd.Run()
-
 fmt.Println ("merging slides and webcams side by side")
-	cmd = exec.Command("ffmpeg", "-i", presentationId+"/"+"slides.mp4",
-				"-i", presentationId+"/"+"webcams.mp4",
+	cmd = exec.Command("ffmpeg", "-i", presentationId+"/"+slidesFile,
+				"-i", presentationId+"/"+"webcams644.mp4",
 				"-filter_complex", "[0:v][1:v]hstack=inputs=2[v]", 
+				"-t", fmt.Sprint(videoLength),
 				"-map", "[v]", "-map", "1:a", meetingName[0]+".mp4")
 	cmd.Run()
 fmt.Println ("Name of the final video is: ", meetingName[0])
- os.RemoveAll(presentationId+"/")  // delete temporary dir
- err = os.Remove("video_list.txt") // delete video-list file
+os.RemoveAll(presentationId+"/")  // delete temporary dir
+err = os.Remove("video_list.txt") // delete video-list file
 }
 
 // DownloadFile will download a url to a local file. 
