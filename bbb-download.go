1efa8ab6a6315ad7dc3461329eedd1666c6708a5
Zafer Akçalı <zaferakcali@yahoo.com>
2021-02-20 00:49:28 +0300

    resolution increased to 1536x768

--- bbb-download.go
+++ bbb-download.go
@@ -24,7 +24,7 @@ func main () {
 	fmt.Print ("Enter url of conference/lecture: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-    presentationUrl := scanner.Text()
+    	presentationUrl := scanner.Text()
 	result := strings.SplitAfter(presentationUrl,"?meetingId=")
 	presentationId := result [1]
 
@@ -41,7 +41,6 @@ func main () {
         log.Fatal(err)
     }
     defer responseMeta.Body.Close()
-
 	metaBody, err := ioutil.ReadAll (responseMeta.Body) 
     if err != nil {
         log.Fatal(err)
@@ -54,26 +53,21 @@ func main () {
     meetingString := strings.SplitAfter(string(metaBody),"<meetingName>")
     meetingName := strings.Split(meetingString[1],"</meetingName>")
     fmt.Println ("name of the meeting=", meetingName[0])
-
-
 	//read content of the shapes.svg file, and assign shapes to it
     responseShapes, err := http.Get(shapesUrl)
     if err != nil {
         log.Fatal(err)
     }
     defer responseShapes.Body.Close()
-
-  	shapesBody, err := ioutil.ReadAll (responseShapes.Body) 
+	shapesBody, err := ioutil.ReadAll (responseShapes.Body) 
     if err != nil {
         log.Fatal(err)
     }
     shapes:=string (shapesBody)
-
    	fmt.Println ("creating directory: ", presentationId)
 	if _, err := os.Stat(presentationId); os.IsNotExist(err) {
     os.Mkdir(presentationId, 0700) // create temporary dir
 }
-
 // download webcams
 	fmt.Print ("downloading webcams",  "\r")
 	if err := DownloadFile(presentationId+"/"+webcamsFile, webcamsUrl+"/"+webcamsFile); err != nil {
@@ -146,22 +140,20 @@ fmt.Println("Length of presentation =",  videoLength)
 truncated = ( videoLength*10-inValue*10)/10
 durations[i-1] = math.Round (truncated*100)/100
 fmt.Println("Duration of last slide according to meta.xml =", durations[i-1]) 
-	
 		// create mp4 files from png files
 fmt.Println ("Creating videos from slide pictures, duration is given as seconds")
 	for j:=1; j<=nSlides; j++ 	{
 	fmt.Print (imgnames[j], " ", vidnames[j], " ", durations [j], "\r") // print to same line just like a counter
 	cmd := exec.Command("ffmpeg","-loop", "1", "-r", "5", "-f", "image2", 
-						"-i", presentationId +"/"+imgnames[j],
-						"-c:v", "libx264", "-r", "24", "-t", fmt.Sprint(durations[j]), "-pix_fmt", "yuv420p", 
-						"-vf", "scale='if(gt(a,1024/768),1024,-2)':'if(gt(a,1024/768),-2,768)',pad=1024:768:(ow-iw)/2:(oh-ih)/2:color=white",        // as close as 800x600
-						presentationId+"/"+vidnames[j]  )
+			"-i", presentationId +"/"+imgnames[j],
+			"-c:v", "libx264", "-r", "24", "-t", fmt.Sprint(durations[j]), "-pix_fmt", "yuv420p", 
+			"-vf", "scale='if(gt(a,1024/768),1024,-2)':'if(gt(a,1024/768),-2,768)',pad=1024:768:(ow-iw)/2:(oh-ih)/2:color=white",        // as close as 800x600
+			presentationId+"/"+vidnames[j]  )
 	cmd.Run()
-
 	}
 
 if nSlides == 1 { 
-			slidesFile="v1.mp4" // none of the videos are merged
+	slidesFile="v1.mp4" // none of the videos are merged
 } else { // there are more than one video file, so merge them	
 	//create video_list.txt file to cancat with ffmpeg
 	    f, err := os.Create("video_list.txt")
@@ -184,25 +176,23 @@ if nSlides == 1 {
 
 fmt.Println ("merging slide videos to create: slides.mp4")
 	cmd := exec.Command("ffmpeg","-f", "concat", "-safe", "0", "-i", "video_list.txt", 
-						"-c", "copy", presentationId+"/"+slidesFile)
+			"-c", "copy", presentationId+"/"+slidesFile)
 	cmd.Run()
 fmt.Println ("slide videos merged")
-
 } 
-
 //convert webcams video file to  webcamsRight.mp4
 fmt.Println ("converting ",webcamsFile, " to  webcamsRight.mp4")
 	cmd := exec.Command("ffmpeg","-i", presentationId+"/"+webcamsFile,
-						 "-q:a", "0", "-q:v", "0", 
-						 "-vf", "scale=512:-2,pad=height=768:color=white", 
-						presentationId+"/"+"webcamsRight.mp4")
+			 "-q:a", "0", "-q:v", "0", 
+			 "-vf", "scale=512:-2,pad=height=768:color=white", 
+			presentationId+"/"+"webcamsRight.mp4")
 	cmd.Run()
 fmt.Println ("merging slides and webcams side by side")
 	cmd = exec.Command("ffmpeg", "-i", presentationId+"/"+slidesFile,
-				"-i", presentationId+"/"+"webcamsRight.mp4",
-				"-filter_complex", "[0:v][1:v]hstack=inputs=2[v]", 
-				"-t", fmt.Sprint(videoLength),
-				"-map", "[v]", "-map", "1:a", meetingName[0]+".mp4")
+			"-i", presentationId+"/"+"webcamsRight.mp4",
+			"-filter_complex", "[0:v][1:v]hstack=inputs=2[v]", 
+			"-t", fmt.Sprint(videoLength),
+			"-map", "[v]", "-map", "1:a", meetingName[0]+".mp4")
 	cmd.Run()
 fmt.Println ("Name of the final video is: ", meetingName[0])
 os.RemoveAll(presentationId+"/")  // delete temporary dir
